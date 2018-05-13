53ad5271c28556d1aed2c57a56a845c7b30c01b1
KingXyen <mushroomforbusinessgmail.com>
2018-05-13 16:10:07 +0100

    FirstWebsite

--- js/index.js
+++ js/index.js
@@ -1,13 +1,43 @@
-$(".form").on('click', function(){
-  $(this).addClass('active');
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
 });
 
-$(".submit").on('click', function() {
-  $(this).parent().parent().hide(300);
-  $(".ok_message").addClass("active");
-});
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
 
-$(".ok_message").on('click', function() {
-  $(this).removeClass("active");
-  $(".form").removeClass("active").show();
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
 });
